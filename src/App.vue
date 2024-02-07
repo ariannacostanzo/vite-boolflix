40e9b82d051544ae15cc660685addd6b1c06272b
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-07 14:30:04 +0100

    mapping results

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,58 @@
   import axios from 'axios';
   import AppHeader from './components/AppHeader.vue';
   import AppMain from './components/AppMain.vue';
+  import {store} from './assets/data/store.js'
 
   const baseUrl = 'https://api.themoviedb.org/3/search/movie?api_key=7a52c80d921a51f7f218955a9737d1d2&language=it-IT'
 
   export default {
     name: 'App',
+    data() {
+      return {
+        searchedTerm: ''
+      }
+    },
     methods: { 
-      fetchData(endpoint) {
+      fetchData(endpoint, container) {
         axios.get(endpoint).then(res => {
-          console.log(res.data.results)
-        })
+          //raccolgo i risultati all'interno di store.Container (ovvero l'array che scelgo)
+          // console.log(res.data.results)
+          store[container] = res.data.results.map((element) => {
+            return {
+              id: element.id,
+              language: element.original_language,
+              title: element.title,
+              originalTitle: element.original_title,
+              vote: element.vote_average,
+              imagePath: element.poster_path,
+              summary: element.overview
+            }
+          })
+          console.log(store.movies)
+
+
+        }).catch(err => console.log(err))
       },
-      readHeader(item) {
-        console.log(item)
+      getSearchText(item) {
+        // console.log(item);
+        this.searchedTerm = item
+        // console.log(this.searchedTerm);
+        const endpoint = baseUrl + `&query=${this.searchedTerm}`
+        this.fetchData(endpoint, 'movies')
       }
 
     },
     components: {
       AppHeader, AppMain
     },
-    created() {
-      this.fetchData(baseUrl + `&query=pippo`)
-    }
   }
 </script>
 
 <template>
-  <AppHeader @text-searched="readHeader"/>
+
+
+
+  <AppHeader @text-searched="getSearchText"/>
   <AppMain />
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -24,6 +24,8 @@
   header {
     display: flex;
     align-items: center;
+    // justify-content: space-between;
+    margin-bottom: 2rem;
 
   }
 </style>
